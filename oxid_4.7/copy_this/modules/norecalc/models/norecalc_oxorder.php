e51c36e10cbd0e341c732d723609ad8545fedaf3
nickname <support@ecomstyle.de>
2013-07-18 08:37:00 +0200

    workflow adjusted
    norecalc is now active when the orderfolder is not "new"

--- oxid_4.7/copy_this/modules/norecalc/models/norecalc_oxorder.php
+++ oxid_4.7/copy_this/modules/norecalc/models/norecalc_oxorder.php
@@ -12,7 +12,7 @@ class norecalc_oxorder extends norecalc_oxorder_parent
     }
     public function hasInvoice()
     {
-        if ($this->oxorder__oxbilldate->value != '0000-00-00'){
+        if ($this->oxorder__oxfolder->value != 'ORDERFOLDER_NEW'){
             return true;
         }
     }
